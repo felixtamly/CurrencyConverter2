00f1417e459fb2f21163fc9e77010ea5dc965f94
Felix Tam <felixtam2004@gmail.com>
2017-11-22 22:09:35 +0000

    To EUR

--- Program.cs
+++ Program.cs
@@ -13,51 +13,116 @@ namespace CurrencyConverter2
 			CurrencyList = XmlConverter.GetCurrencyList();
             RateFinder RateFinder = new RateFinder(CurrencyList);
             FromEURConverter FromEURConverter = new FromEURConverter(CurrencyList);
+            ToEURConverter ToEURConverter = new ToEURConverter(CurrencyList);
             int ChoiceInput = 0;
-            double ValueInput;
-            string NameInput;
-            double ValueResult;
+            double ValueInput = 0.0;
+            string NameInput = null;
+            double ValueResult = 0.0;
 
             //MENU
             Console.WriteLine("*****MENU*****");
             Console.WriteLine("1. View available currencies");
             Console.WriteLine("2. Find exchange rate");
             Console.WriteLine("3. Convert from Euro");
+            Console.WriteLine("4. Convert to Euro");
+            Console.WriteLine("5. Log out");
             try
             {
 				ChoiceInput = Int32.Parse(Console.ReadLine());
             } catch (FormatException e)
             {
+                Console.WriteLine("Your input was invalid.");
                 Main(args);
             }
 
-            if (ChoiceInput == 1)
+            switch (ChoiceInput)
             {
-				foreach (var Currency in CurrencyList)
-				{
-					Console.WriteLine(Currency);
-				}
-            }
-
-            else if (ChoiceInput == 2)
-            {
-				Console.WriteLine("Enter currency: ");
-				NameInput = Console.ReadLine();
-				Console.WriteLine(RateFinder.FindRate(NameInput.ToUpper()));            
-            }
-
-            else if (ChoiceInput == 3)
-            {
-                Console.WriteLine("Enter currency: ");
-                NameInput = Console.ReadLine();
-                Console.WriteLine("Enter amount in Euro: ");
-                ValueInput = Double.Parse(Console.ReadLine());
-                ValueResult = FromEURConverter.ConvertFromEUR(NameInput.ToUpper(), ValueInput);
-                Console.WriteLine("You can get {0} {1} from {2} EUR.", Math.Round(ValueResult, 2), NameInput.ToUpper(), Math.Round(ValueInput, 2));
-            }
-            else
-            {
-                Console.WriteLine("Your input was invalid.");
+                //1. View available currencies
+                case 1:
+                    foreach (var Currency in CurrencyList)
+                    {
+                        Console.WriteLine(Currency);
+                    }
+                    Main(args);
+                    break;
+                //2. Find exchange rate
+                case 2:
+                    Console.WriteLine("Enter currency: ");
+                    NameInput = Console.ReadLine();
+                    if(NameInput.ToUpper().Equals("EUR"))
+                    {
+                        Console.WriteLine("1 EUR = 1 EUR");
+                    }
+                    else if(!RateFinder.CurrencyExists(NameInput.ToUpper()))
+                    {
+                        Console.WriteLine("Your input was invalid");
+                    } else
+                    {
+						Console.WriteLine("1 EUR = {0} {1}", RateFinder.FindRate(NameInput.ToUpper()), NameInput.ToUpper());
+                    }
+					Main(args);   
+                    break;
+                //3. Convert from Euro
+                case 3:
+                    Console.WriteLine("Enter currency: ");
+                    NameInput = Console.ReadLine();
+                    if (!RateFinder.CurrencyExists(NameInput.ToUpper()))
+                    {
+                        Console.WriteLine("Your input was invalid");
+                        Main(args);
+                    }
+                    else
+                    {
+						Console.WriteLine("Enter amount in Euro: ");
+						try
+						{
+							ValueInput = Double.Parse(Console.ReadLine());
+						}
+						catch (FormatException e)
+						{
+							Console.WriteLine("Your input was invalid.");
+							Main(args);
+						}
+						ValueResult = FromEURConverter.ConvertFromEUR(NameInput.ToUpper(), ValueInput);
+						Console.WriteLine("You can get {0} {1} from {2} EUR.", Math.Round(ValueResult, 2), NameInput.ToUpper(), Math.Round(ValueInput, 2));
+						Main(args);
+                        
+                    }
+                    break;
+                //4. Convert to Euro
+                case 4:
+                    Console.WriteLine("Enter currency: ");
+                    NameInput = Console.ReadLine();
+                    if(!RateFinder.CurrencyExists(NameInput.ToUpper()))
+                    {
+                        Console.WriteLine("Your input was invalid");
+                        Main(args);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Enter amount in your currency: ");
+                        try
+                        {
+                            ValueInput = Double.Parse(Console.ReadLine());
+                        }
+                        catch (FormatException e)
+                        {
+                            Console.WriteLine("Your input was invalid");
+                            Main(args);
+                        }
+                        ValueResult = ToEURConverter.ConvertToEUR(NameInput.ToUpper(), ValueInput);
+                        Console.WriteLine("You can get {0} EUR from {1} {2}.", Math.Round(ValueResult, 2), Math.Round(ValueInput, 2), NameInput);
+                        Main(args);
+                    }
+                    break;
+                //5. Log out
+                case 5:
+                    Environment.Exit(1);
+                    break;
+                default:
+                    Console.WriteLine("Your input was invalid.");
+                    Main(args);
+                    break;
             }
         }
     }
---
+++ ToEURConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CurrencyConverter2
+{
+    public class ToEURConverter
+    {
+        readonly List<Currency> CurrencyList;
+        public ToEURConverter(List<Currency> CurrencyList)
+        {
+            this.CurrencyList = CurrencyList;
+        }
+        public double ConvertToEUR(string Name, double Amount)
+        {
+            RateFinder RateFinder = new RateFinder(CurrencyList);
+            double Rate = RateFinder.FindRate(Name);
+            return Amount / Rate;
+        }
+    }
+}
